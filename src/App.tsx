5d93d29106027d09ab5308dec98077d7881ce737
Samir Karimov <karimovsamir93@gmail.com>
2025-09-08 17:54:33 +0400

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. No, learn BackEnd
+          Learn React. No, learn Back
         </a>
       </header>
     </div>
